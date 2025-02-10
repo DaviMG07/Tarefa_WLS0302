f28c269ee554d440ae675b4c1fc949b1c3495adb
Davi <ivad162@gmail.com>
2025-02-09 23:23:18 -0300

    remoção da pasta build

--- build/CMakeFiles/Tarefa_WLS0302_ws2812_pio_h.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for custom commands dependencies management for Tarefa_WLS0302_ws2812_pio_h.
--- build/CMakeFiles/serial_ws2812_pio_h.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "/home/davi/Projects/Tarefa_WLS0302/lib/ws2812.pio.h"
-  "CMakeFiles/serial_ws2812_pio_h"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/serial_ws2812_pio_h.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
